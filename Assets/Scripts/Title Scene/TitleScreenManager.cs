c2a7b0ff864dd472fe1a7392933f194b906b8b09
Yonni <92130779+YonniGashu@users.noreply.github.com>
2024-09-21 18:02:09 -0400

    Save game part 2 completed

--- Assets/Scripts/Title Scene/TitleScreenManager.cs
+++ Assets/Scripts/Title Scene/TitleScreenManager.cs
@@ -1,13 +1,23 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using Unity.Netcode;
+using Unity.VisualScripting;
+using UnityEngine.UI;
 
 namespace YG
 {
     public class TitleScreenManager : MonoBehaviour 
     { 
+        [Header("Menus")]
+        [SerializeField] GameObject titleScreenMainMenu;
+        [SerializeField] GameObject titleScreenLoadMenu;
+
+        [Header("Buttons")]
+        [SerializeField] Button loadMenuReturnButton;
+        [SerializeField] Button mainMenuLoadGameButton;
+
         public void StartNetworkAsHost()
         {
             NetworkManager.Singleton.StartHost();
@@ -17,6 +27,28 @@ namespace YG
         {
             WorldSaveGameManager.instance.CreateNewGame();
             StartCoroutine(WorldSaveGameManager.instance.LoadWorldScene());
+        }
+
+        public void OpenLoadGameMenu() {
+            // CLOSE MAIN MENU
+            titleScreenMainMenu.SetActive(false);
+
+            // OPEN LOAD MENU
+            titleScreenLoadMenu.SetActive(true);
+
+            // SELECT THE RETURN BUTTON
+            loadMenuReturnButton.Select();
+        }
+
+        public void ReturnToMainMenu() {
+            // CLOSE MAIN MENU
+            titleScreenLoadMenu.SetActive(false);
+
+            // OPEN LOAD MENU
+            titleScreenMainMenu.SetActive(true);
+
+            // SELECT THE MAIN MENU LOAD GAME BUTTON
+            mainMenuLoadGameButton.Select();
         }
     }
     
--- Assets/Scripts/World Managers/WorldSaveGameManager.cs
+++ Assets/Scripts/World Managers/WorldSaveGameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -28,15 +29,17 @@ namespace YG
 
         [Header("Character Slots")]
         public CharacterSaveData characterSlot01;
-        //public CharacterSaveData characterSlot02;
-        //public CharacterSaveData characterSlot03;
-        //public CharacterSaveData characterSlot04;
-        //public CharacterSaveData characterSlot05;
-        //public CharacterSaveData characterSlot06;
-        //public CharacterSaveData characterSlot07;
-        //public CharacterSaveData characterSlot08;
-        //public CharacterSaveData characterSlot09;
-        //public CharacterSaveData characterSlot10
+        public CharacterSaveData characterSlot02;
+        public CharacterSaveData characterSlot03;
+        public CharacterSaveData characterSlot04;
+        public CharacterSaveData characterSlot05;
+        public CharacterSaveData characterSlot06;
+        public CharacterSaveData characterSlot07;
+        public CharacterSaveData characterSlot08;
+        public CharacterSaveData characterSlot09;
+        public CharacterSaveData characterSlot10;
+
+
 
         private void Awake()
         {
@@ -54,6 +57,7 @@ namespace YG
         private void Start()
         {
             DontDestroyOnLoad(gameObject);
+            LoadAllCharacterSlots();
         }
 
         private void Update()
@@ -71,50 +75,51 @@ namespace YG
             }
         }
 
-        private void DecideCharacterFileNameBasedOnCharacterSlotBeingUsed()
+        public string DecideCharacterFileNameBasedOnCharacterSlotBeingUsed(CharacterSlot characterSlot)
         {
-            switch (currentCharacterSlotBeingUsed)
+            String fileName = "";
+            switch (characterSlot)
             {
                 case CharacterSlot.CharacterSlot_01:
-                    saveFileName = "characterSlot_01";
+                    fileName = "characterSlot_01";
                     break;
                 case CharacterSlot.CharacterSlot_02:
-                    saveFileName = "characterSlot_02";
+                    fileName = "characterSlot_02";
                     break;
                 case CharacterSlot.CharacterSlot_03:
-                    saveFileName = "characterSlot_03";
+                    fileName = "characterSlot_03";
                     break;
                 case CharacterSlot.CharacterSlot_04:
-                    saveFileName = "characterSlot_04";
+                    fileName = "characterSlot_04";
                     break;
                 case CharacterSlot.CharacterSlot_05:
-                    saveFileName = "characterSlot_05";
+                    fileName = "characterSlot_05";
                     break;
                 case CharacterSlot.CharacterSlot_06:
-                    saveFileName = "characterSlot_06";
+                    fileName = "characterSlot_06";
                     break;
                 case CharacterSlot.CharacterSlot_07:
-                    saveFileName = "characterSlot_07";
+                    fileName = "characterSlot_07";
                     break;
                 case CharacterSlot.CharacterSlot_08:
-                    saveFileName = "characterSlot_08";
+                    fileName = "characterSlot_08";
                     break;
                 case CharacterSlot.CharacterSlot_09:
-                    saveFileName = "characterSlot_09";
+                    fileName = "characterSlot_09";
                     break;
                 case CharacterSlot.CharacterSlot_10:
-                    saveFileName = "characterSlot_10";
+                    fileName = "characterSlot_10";
                     break;
                 default:
                     break;
-
             }
+            return fileName;
         }
 
         public void CreateNewGame()
         {
             // CREATE A NEW FILE WITH A FILE NAME DEPENDING ON WHICH SLOT YOU'RE USING
-            DecideCharacterFileNameBasedOnCharacterSlotBeingUsed();
+            DecideCharacterFileNameBasedOnCharacterSlotBeingUsed(currentCharacterSlotBeingUsed);
 
             currentCharacterData = new CharacterSaveData();
         }
@@ -122,7 +127,7 @@ namespace YG
         public void LoadGame ()
         {
             // LOADING A FILE WITH A FILE NAME DEPENDING ON WHICH SLOT YOU'RE USING
-            DecideCharacterFileNameBasedOnCharacterSlotBeingUsed();
+            saveFileName = DecideCharacterFileNameBasedOnCharacterSlotBeingUsed(currentCharacterSlotBeingUsed);
 
             saveFileDataWriter = new SaveFileDataWriter();
             // GENERALLY WORKS ON MULTIPLE MACHINE (Application.persistentDataPath)
@@ -136,7 +141,7 @@ namespace YG
         public void SaveGame()
         {
             // SAVE THE CURRENT FILE UNDER A FILE NAME DEPENDING ON WHICH SLOT WE ARE USING
-            DecideCharacterFileNameBasedOnCharacterSlotBeingUsed();
+            saveFileName = DecideCharacterFileNameBasedOnCharacterSlotBeingUsed(currentCharacterSlotBeingUsed);
 
             saveFileDataWriter = new SaveFileDataWriter();
             // GENERALLY WORKS ON MULTIPLE MACHINE (Application.persistentDataPath)
@@ -148,6 +153,43 @@ namespace YG
 
             // WRITE THAT INFO ONTO A JSON FILE, SAVED TO THIS MACHINE
             saveFileDataWriter.CreateNewCharacterSaveFile(currentCharacterData);
+        }
+
+        // LOAD ALL CHARACTER PROFILES ON DEVICE WHEN STARTING GAME
+        private void LoadAllCharacterSlots() 
+        {
+            saveFileDataWriter = new SaveFileDataWriter();
+            saveFileDataWriter.saveDataDirectoryPath = Application.persistentDataPath;
+
+            saveFileDataWriter.saveFileName = DecideCharacterFileNameBasedOnCharacterSlotBeingUsed(CharacterSlot.CharacterSlot_01);
+            characterSlot01 = saveFileDataWriter.LoadSaveFile();
+
+            saveFileDataWriter.saveFileName = DecideCharacterFileNameBasedOnCharacterSlotBeingUsed(CharacterSlot.CharacterSlot_02);
+            characterSlot02 = saveFileDataWriter.LoadSaveFile();
+
+            saveFileDataWriter.saveFileName = DecideCharacterFileNameBasedOnCharacterSlotBeingUsed(CharacterSlot.CharacterSlot_03);
+            characterSlot03 = saveFileDataWriter.LoadSaveFile();
+
+            saveFileDataWriter.saveFileName = DecideCharacterFileNameBasedOnCharacterSlotBeingUsed(CharacterSlot.CharacterSlot_04);
+            characterSlot04 = saveFileDataWriter.LoadSaveFile();
+
+            saveFileDataWriter.saveFileName = DecideCharacterFileNameBasedOnCharacterSlotBeingUsed(CharacterSlot.CharacterSlot_05);
+            characterSlot05 = saveFileDataWriter.LoadSaveFile();
+
+            saveFileDataWriter.saveFileName = DecideCharacterFileNameBasedOnCharacterSlotBeingUsed(CharacterSlot.CharacterSlot_06);
+            characterSlot06 = saveFileDataWriter.LoadSaveFile();
+
+            saveFileDataWriter.saveFileName = DecideCharacterFileNameBasedOnCharacterSlotBeingUsed(CharacterSlot.CharacterSlot_07);
+            characterSlot07 = saveFileDataWriter.LoadSaveFile();
+
+            saveFileDataWriter.saveFileName = DecideCharacterFileNameBasedOnCharacterSlotBeingUsed(CharacterSlot.CharacterSlot_08);
+            characterSlot08 = saveFileDataWriter.LoadSaveFile();
+
+            saveFileDataWriter.saveFileName = DecideCharacterFileNameBasedOnCharacterSlotBeingUsed(CharacterSlot.CharacterSlot_09);
+            characterSlot09 = saveFileDataWriter.LoadSaveFile();
+
+            saveFileDataWriter.saveFileName = DecideCharacterFileNameBasedOnCharacterSlotBeingUsed(CharacterSlot.CharacterSlot_10);
+            characterSlot10 = saveFileDataWriter.LoadSaveFile();
         }
 
         public IEnumerator LoadWorldScene()
